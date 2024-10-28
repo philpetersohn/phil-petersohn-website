0258a6da1d35fd69ffb03a9a08d6498c37373a73
philpetersohn <petersohn.phil@gmail.com>
2024-10-28 13:22:08 -0700

    fix

--- my-website/src/components/Navbar.jsx
+++ my-website/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import me from "../assets/Picture4.svg"; // Correctly import the image
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -9,13 +10,17 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-blue-100 text-blue-600 flex justify-between items-center p-4 w-full">
-      <div className="flex-grow flex justify-start">
-        <button className="text-xl font-bold">
-          <Link to="/home" className="">
-            PP
-          </Link>
-        </button>
+    <nav className="bg-blue-100 text-blue-600 flex justify-between items-center p-4 w-full shadow-md">
+      <div className="flex-grow flex justify-start lg:pl-80 md:pl-10">
+        <Link to="/home" className="pl-4 md:pl-0">
+          <figure>
+            <img
+              src={me}
+              alt="Profile"
+              className="w-10 h-10 md:w-20 md:h-20 rounded-full"
+            />
+          </figure>
+        </Link>
       </div>
       <div className="relative">
         <button className="block md:hidden" onClick={toggleDropdown}>
@@ -29,7 +34,7 @@ function Navbar() {
           <li className="border-b border-blue-700 md:border-none w-full">
             <Link
               to="/home"
-              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center"
+              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center font-semibold"
             >
               Home
             </Link>
@@ -37,7 +42,7 @@ function Navbar() {
           <li className="border-b border-blue-700 md:border-none w-full">
             <Link
               to="/about"
-              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center"
+              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center font-semibold"
             >
               About
             </Link>
@@ -45,7 +50,7 @@ function Navbar() {
           <li className="border-b border-blue-700 md:border-none w-full">
             <Link
               to="/projects"
-              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center"
+              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center font-semibold"
             >
               Projects
             </Link>
@@ -53,7 +58,7 @@ function Navbar() {
           <li className="border-b border-blue-700 md:border-none w-full">
             <Link
               to="/contact"
-              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center"
+              className="block p-4 hover:text-blue-300 hover:scale-105 transition-transform w-full text-center font-semibold"
             >
               Contact
             </Link>
