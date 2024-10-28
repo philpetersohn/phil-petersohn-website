ba312dcdd0a58c19a50ff2c9722a5cad80de5a94
philpetersohn <petersohn.phil@gmail.com>
2024-10-28 07:17:33 -0700

    Finish languages page

--- my-website/src/components/Languages.jsx
+++ my-website/src/components/Languages.jsx
@@ -2,30 +2,16 @@ import { FaLanguage } from "react-icons/fa";
 
 function Languages() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen">
-      <div className="text-center">
-        <h1 className="text-blue-600 text-5xl font-bold mb-6 mt-32">
-          Languages
-        </h1>
-        <div className="flex items-center mb-6">
-          <FaLanguage className="text-blue-600 text-3xl mr-2" />
-          <h2 className="text-blue-600 text-3xl font-bold">
-            Languages I Speak
-          </h2>
-        </div>
-        <ul className="list-disc list-inside space-y-2 text-xl mb-6">
-          <li className="flex items-start">
-            <span className="inline-block">English</span>
-          </li>
-          <li className="flex items-start">
-            <span className="inline-block">German</span>
-          </li>
-          <li className="flex items-start">
-            <span className="inline-block">Spanish</span>
-          </li>
-        </ul>
+    <div className="w-full max-w-4xl">
+      <div className="flex items-center mb-6">
+        <FaLanguage className="text-blue-600 text-3xl mr-2" />
+        <h2 className="text-blue-600 text-3xl font-bold">Languages I Speak</h2>
       </div>
-    </section>
+      <ul className="list-disc space-y-2 text-xl mb-6">
+        <li>English</li>
+        <li>German</li>
+      </ul>
+    </div>
   );
 }
 
