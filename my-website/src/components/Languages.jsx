c10fa729b9519eb3e72e8ebdc20cd5508b0fc144
philpetersohn <petersohn.phil@gmail.com>
2024-10-28 10:50:30 -0700

    Fix languages bullet points

--- my-website/src/components/Languages.jsx
+++ my-website/src/components/Languages.jsx
@@ -8,10 +8,12 @@ function Languages() {
         <FaLanguage className="text-blue-600 text-3xl mr-2" />
         <h2 className="text-blue-600 text-3xl font-bold">Languages</h2>
       </div>
-      <ul className="list-disc space-y-2 text-lg mb-6 blue-bullets">
-        <li>English</li>
-        <li>German</li>
-      </ul>
+      <div className="flex items-start">
+        <ul className="list-disc space-y-2 text-lg mb-6 blue-bullets pl-8">
+          <li>English</li>
+          <li>German</li>
+        </ul>
+      </div>
     </div>
   );
 }
